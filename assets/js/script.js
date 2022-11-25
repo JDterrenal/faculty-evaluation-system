19eeffe3d3f238d1befd04a66c345f83f83d64ad
redpangilinan <redpangilinan715@gmail.com>
2022-11-25 18:08:29 +0800

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -67,9 +67,3 @@ function EditFunction() {
         editBackground.style.display = "flex";
     }
 }
-
-function confirmationDelete(anchor) {
-    var conf = confirm('Are you sure want to delete this record?');
-    if (conf)
-        window.location = anchor.attr("href");
-}
