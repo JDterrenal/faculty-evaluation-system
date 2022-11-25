066e76f215adad4208648d856ac81f27f44e296e
redpangilinan <redpangilinan715@gmail.com>
2022-11-25 17:00:16 +0800

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -66,4 +66,10 @@ function EditFunction() {
     } else {
         editBackground.style.display = "flex";
     }
-};
+}
+
+function confirmationDelete(anchor) {
+    var conf = confirm('Are you sure want to delete this record?');
+    if (conf)
+        window.location = anchor.attr("href");
+}
