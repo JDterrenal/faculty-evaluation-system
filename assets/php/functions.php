b7061caeaa621d1c7d66c61081ed5c2ee236acbd
redpangilinan <redpangilinan715@gmail.com>
2022-11-25 20:48:35 +0800

    Update functions.php

--- assets/php/functions.php
+++ assets/php/functions.php
@@ -296,7 +296,7 @@ function addCourse()
         $sql = "INSERT INTO tb_courses (course_id, course_name) VALUES (null, '$course_name')";
         if (mysqli_query($conn, $sql)) {
             echo "Course Added!";
-            header('Location: /assets/php/loader.php');
+            header('Location: '.$_SERVER['REQUEST_URI']);
         } else {
             echo "Invalid input!";
         }
