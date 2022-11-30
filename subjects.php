802610915e9c5c977b9566fb519f77bebb4e2864
redpangilinan <redpangilinan715@gmail.com>
2022-11-30 17:56:45 +0800

    Update subjects.php

--- subjects.php
+++ subjects.php
@@ -12,6 +12,7 @@ enableDelete_subjects();
   <link rel="icon" href="images/logo.png" type="image/x-icon" />
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/css/all.min.css">
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js" charset="utf-8"></script>
+  <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
   <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
   <link rel="stylesheet" href="./assets/css/style.css">
 </head>
@@ -33,15 +34,23 @@ enableDelete_subjects();
 							</div>
 						</div>
 						<div class="user-nfo-content">
-						<table class="user-table">
+							<table class="user-table">
 								<tbody>
 									<tr>
 										<th>ID Info</th>
 										<td data-label="ID Info"></td>
 									</tr>
 									<tr>
+										<th>Subject Code Info</th>
+										<td data-label="Subject Code Info"></td>
+									</tr>
+									<tr>
 										<th>Subject Info</th>
 										<td data-label="Subject Info"></td>
+									</tr>
+									<tr>
+										<th>Units Info</th>
+										<td data-label="Units Info"></td>
 									</tr>
 								<tbody>
 							</table>
@@ -67,7 +76,9 @@ enableDelete_subjects();
 								<thead>
 									<tr>
 										<th>ID</th>
+										<th>Subject Code</th>
 										<th>Subject</th>
+										<th>Units</th>
 										<th>Operation</th>
 									</tr>
 								</thead>
@@ -98,8 +109,18 @@ enableDelete_subjects();
 					<i class="fas fa-times ex" id="ex-add"></i>
 				</div>
 				<div class="popup-add-middle">
-					<p class="label1">Subject Name</p>
-					<input type="text" name="subject_name" placeholder="Subject Name" class="course-name-tbx" required>
+					<div class="boxx">
+						<p class="label1">Subject Code</p>
+						<input type="text" name="subject_code" placeholder="Subject Code" class="course-name-tbx" required>
+					</div>
+					<div class="boxx">
+						<p class="label1">Subject Name</p>
+						<input type="text" name="subject_name" placeholder="Subject Name" class="course-name-tbx" required>
+					</div>
+					<div class="boxx">
+						<p class="label1">Units</p>
+						<input type="number" name="units" placeholder="Units" class="course-name-tbx" required>
+					</div>
 					<?php addSubject() ?>
 					<button type="submit" name="addsubject" class="addbtn"><i class="fas fa-plus"></i> Add Subject</button>
 				</div>
@@ -117,8 +138,18 @@ enableDelete_subjects();
 				</div>
 				<div class="popup-add-middle">
 					<input type="hidden" name="edit_id" id="edit_id">
-					<p class="label1">Subject Name</p>
-					<input type="text" name="edit_subject_name" id="edit_subject_name" placeholder="Subject Name" class="editcourse-name-tbx" required>
+					<div class="boxx">
+						<p class="label1">Subject Code</p>
+						<input type="text" name="edit_subject_code" id="edit_subject_code" placeholder="Subject Name" class="editcourse-name-tbx" required>
+					</div>
+					<div class="boxx">
+						<p class="label1">Subject Name</p>
+						<input type="text" name="edit_subject_name" id="edit_subject_name" placeholder="Subject Name" class="editcourse-name-tbx" required>
+					</div>
+					<div class="boxx">
+						<p class="label1">Units</p>
+						<input type="number" name="edit_units" id="edit_units" placeholder="Subject Name" class="editcourse-name-tbx" required>
+					</div>
 					<?php editSubject() ?>
 					<button type="submit" name="editsubject" class="editbtn"><i class="fas fa-edit"></i> Edit Subject</button>
 				</div>
