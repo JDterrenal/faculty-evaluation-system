56fbb99cecc6cf31140b412686a5a164b38bd97d
JDterrenal <johnderickramosterrenal@gmail.com>
2022-12-02 18:11:58 +0800

    up

---
+++ edit_question.php
@@ -0,0 +1,58 @@
+<?php
+include './assets/php/functions.php';
+preventBack();
+enableDelete_courses();
+?>
+
+<html>
+<head>
+<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Courses | Faculty Evaluation</title>
+  <script src = "https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <link rel="icon" href="images/logo.png" type="image/x-icon" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/css/all.min.css">
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js" charset="utf-8"></script>
+  <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+  <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
+  <link rel="stylesheet" href="./assets/css/style.css">
+</head>
+<body>
+	<!------ navigation and side bar ------->
+	<?php include './assets/php/navigation.php' ?>
+	
+	<!-------- main content ---------->
+	<div class= "main-container">
+		<main>
+			<div class="container-main">
+				<div class="page-container"><h1><i class="fas fa-book" id="view-info"> Manage Question</i></h1>
+					<hr>
+					<div class="question-form">
+                        <div class="question-form-top">
+                            <p class="question-form-title">Question Form</p>
+                        </div>
+                        <div class="question-form-middle">
+                            <p class="label">Question</p>
+                            <textarea class="question">Enter text here...</textarea>
+                        </div>
+                        <div class="question-form-bottom">
+                            <a class='save'>Save</a>
+                            <a class='Cancel'>Cancel</a>
+                        </div>
+                    </div>
+				</div>
+			</div>
+		</main>
+
+		<!-------- footer ---------->
+		<?php include './assets/php/footer.php' ?>
+	</div>
+	
+	<!--================= popups ===================-->
+	<!-------- popup logout ---------->
+	<?php include './assets/php/popupLogout.php' ?>
+
+	<script src="./assets/js/script.js"></script>
+	<script src="./assets/js/deleteConfirmation.js"></script>
+</body>
+</html>
