277ba36c222512dc478509644d30542aa38ab557
JDterrenal <johnderickramosterrenal@gmail.com>
2022-12-06 15:00:17 +0800

    up

--- student_evaluation.php
+++ student_evaluation.php
@@ -79,7 +79,7 @@ enableDelete_courses();
                                     </thead>
                                     <tbody>
                                         <tr>
-                                            <td>Maliit ang tite ni uriel</td>
+                                            <td>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</td>
                                             <td data-label='Stongly Disagree'><input type="radio" name="likert" value="strong_disagree"></td>
                                             <td data-label='Disagree'><input type="radio" name="likert" value="disagree"></td>
                                             <td data-label='Uncertain'><input type="radio" name="likert" value="uncertain"></td>
