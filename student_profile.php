b518468431a588214714a186c6ce4bdd205bce75
redpangilinan <redpangilinan715@gmail.com>
2022-12-11 23:24:23 +0800

    Renamed title

--- student_profile.php
+++ student_profile.php
@@ -7,7 +7,7 @@ preventBack();
 <head>
 <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Students | Faculty Evaluation</title>
+  <title>Profile | Faculty Evaluation</title>
   <link rel="icon" href="images/logo.png" type="image/x-icon" />
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/css/all.min.css">
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js" charset="utf-8"></script>
