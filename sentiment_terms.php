56e1c4aba7efefe23b1d566f723a828e66af128a
redpangilinan <redpangilinan715@gmail.com>
2022-12-11 22:10:43 +0800

    Can now add data for sentiment terms

--- sentiment_terms.php
+++ sentiment_terms.php
@@ -7,27 +7,43 @@ $usertype = $_SESSION['usertype'];
 <html>
 
 <head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Dashboard | Faculty Evaluation</title>
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-	<link rel="icon" href="images/logo.png" type="image/x-icon" />
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/css/all.min.css">
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js" charset="utf-8"></script>
-	<link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
-	<link rel="stylesheet" href="./assets/css/style.css">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard | Faculty Evaluation</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <link rel="icon" href="images/logo.png" type="image/x-icon" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/css/all.min.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js" charset="utf-8"></script>
+    <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
+    <link rel="stylesheet" href="./assets/css/style.css">
 </head>
 
 <body>
-	<!------ navigation and side bar ------->
-	<?php include './assets/php/navigation.php' ?>
+    <!------ navigation and side bar ------->
+    <?php include './assets/php/navigation.php' ?>
 
-	<!-------- main content ---------->
-	<div class="main-container">
-		<main>
-			<div class="container-main">
-                <div class="page-container"><h1><i class="fas fa-book"> Manage Sentiment Terms</i></h1>
-                     <hr>
+    <!-------- main content ---------->
+    <div class="main-container">
+        <main>
+            <div class="container-main">
+                <div class="page-container">
+                    <h1><i class="fas fa-book"> Manage Sentiment Terms</i></h1>
+                    <hr>
+                    <div class="form">
+                        <form action="edit_questions.php" method="post">
+                            <div class="form-top">
+                                <p class="form-title">Sentiment Terms</p>
+                            </div>
+                            <div class="form-middle">
+                                <p class="label-question">Comment</p>
+                                <textarea name="comment" class="question"></textarea>
+                            </div>
+                            <div class="form-bottom">
+                                <?php addSentiment() ?>
+                                <button type="submit" name="addsentiment" class="save">Submit</button>
+                            </div>
+                        </form>
+                    </div>
                     <div class="sentiment-container">
                         <table class="question-table">
                             <thead>
@@ -64,20 +80,20 @@ $usertype = $_SESSION['usertype'];
                                 <?php displayNeutral() ?>
                             </tbody>
                         </table>
-                    </div>     
+                    </div>
                 </div>
-			</div>
-		</main>
+            </div>
+        </main>
 
-		<!-------- footer ---------->
-		<?php include './assets/php/footer.php' ?>
-	</div>
+        <!-------- footer ---------->
+        <?php include './assets/php/footer.php' ?>
+    </div>
 
-	<!--================= popups ===================-->
-	<!-------- popup logout ---------->
-	<?php include './assets/php/popupLogout.php' ?>
+    <!--================= popups ===================-->
+    <!-------- popup logout ---------->
+    <?php include './assets/php/popupLogout.php' ?>
 
-	<script src="./assets/js/script.js"></script>
+    <script src="./assets/js/script.js"></script>
 </body>
 
 </html>
