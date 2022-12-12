3434186f531f4f00c958f079ca56909eaf1d1d4c
JDterrenal <johnderickramosterrenal@gmail.com>
2022-12-12 21:56:25 +0800

    up

--- index.php
+++ index.php
@@ -18,30 +18,37 @@ autoLogin();
 <body>
     <form action=index.php method=post>
         <div class="login-container">
-            <div class="login-box">
-                <div class="login-content">
-                    <div class="login-header">
-                        <h2>Sign In</h2>
-                    </div>
-                    <div class="login-items">
-                        <label for="login_id">User ID</label>
-                        <input type="number" placeholder="Enter ID" name="login_id">
-                    </div>
-                    <div class="login-items marg-b">
-                        <label for="password">Password</label>
-                        <input type="password" placeholder="Enter Password" name="password">
-                        <?php login() ?>
-                    </div>
-                    <div class="login-items marg-b">
-                        <button type="submit" name="login" value="Login">Sign In</button>
-                    </div>
-                    <div class="login-type">
-                        <label for="usertype">Sign In as</label>
-                        <select name="usertype" id="usertype">
-                            <option value="Student">Student</option>
-                            <option value="Faculty">Faculty</option>
-                            <option value="Admin">Admin</option>
-                        </select>
+            <div class="login-left">
+                <p class="login-title">Harvard College</p>
+                <p class="login-quote">Education is the passport to the future, for tomorrow belongs to those who prepare for it today</p>
+            </div>
+
+            <div class="login-right">
+                <div class="login-box">
+                    <div class="login-content">
+                        <div class="login-header">
+                            <h2>Sign In</h2>
+                        </div>
+                        <div class="login-items">
+                            <label for="login_id">User ID</label>
+                            <input type="number" placeholder="Enter ID" name="login_id">
+                        </div>
+                        <div class="login-items marg-b">
+                            <label for="password">Password</label>
+                            <input type="password" placeholder="Enter Password" name="password">
+                            <?php login() ?>
+                        </div>
+                        <div class="login-items marg-b">
+                            <button type="submit" name="login" value="Login">Sign In</button>
+                        </div>
+                        <div class="login-type">
+                            <label for="usertype">Sign In as</label>
+                            <select name="usertype" id="usertype">
+                                <option value="Student">Student</option>
+                                <option value="Faculty">Faculty</option>
+                                <option value="Admin">Admin</option>
+                            </select>
+                        </div>
                     </div>
                 </div>
             </div>
